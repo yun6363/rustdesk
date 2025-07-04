6b300ff53699cf9cf79051bdee1490848730c0fd
yun6363 <181936769@qq.com>
2025-07-04 13:54:52 +0800

    Update common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://218.27.16.117:21114".to_owned()
 }
 
 #[inline]
